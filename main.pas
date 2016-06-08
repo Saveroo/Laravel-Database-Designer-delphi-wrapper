c4badf94c7b0744f5e1ee5eb33b08bdc4c52e072
saveroo <saveroo27@gmail.com>
2016-06-08 15:13:54 -0400

    Initial

---
+++ main.pas
@@ -0,0 +1,31 @@
+unit main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.WebBrowser;
+
+type
+  TForm1 = class(TForm)
+    WebBrowser1: TWebBrowser;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+ WebBrowser1.URL := 'http://biodesignrealworld.github.io/LaravelDatabaseDesigner/';
+end;
+
+end.
